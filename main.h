662fac2dac5622cca5635fe23932b91a4711d43a
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-23 02:38:35 +0000

    rename print.h file to main.h file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_string(char *s);
+int _main(void);
+
+#endif
