1b825f9823f705dd4d5a22a0b4425fbcf9eeccff
AyomideKayode <ayomidekay7@gmail.com>
2023-07-23 17:27:24 +0000

    Modified _string.c and _printf.c files.

--- _printf.c
+++ _printf.c
@@ -14,35 +14,39 @@ int _printf(const char *format, ...)
 
 	va_start(args_p, format);
 
-	while (format[fmt_idx])
+	for (; format[fmt_idx] != '\0'; fmt_idx++)
+	//while (format[fmt_idx])
 	{
 
 		if (format[fmt_idx] != '%')
 		{
 			_putchar(format[fmt_idx]);
-			n_char++;
-			fmt_idx++;
+			//n_char++;
+			//fmt_idx++;
 		}
 		else if (format[fmt_idx + 1] == 'c')
 		{
 			_putchar(va_arg(args_p, int));
-			fmt_idx += 2;
-			n_char += 1;
+			fmt_idx++;
+			//fmt_idx += 2;
+			//n_char += 1;
 		}
 		else if (format[fmt_idx + 1] == 's')
 		{
 			s_val = _print_string(va_arg(args_p, char *));
-			n_char += s_val;
-			fmt_idx += 2;
+			fmt_idx++;
+			n_char += (s_val - 1);
+/* removing 1 from string value function because we still increment the n_char in for loop */
+			//fmt_idx += 2;
 		}
 		else if (format[fmt_idx + 1] == '%')
 		{
 			_putchar('%');
-			fmt_idx += 2;
-			n_char++;
+			fmt_idx++;
+			//fmt_idx += 2;
+			//n_char++;
 		}
-
-
+		n_char += 1;
 	}
 	va_end(args_p);
 	return (n_char);
--- _string.c
+++ _string.c
@@ -4,15 +4,17 @@
 
 int _print_string(char *s)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, str_val = 0;
 
 	if (s)
 	{
-		while (s[i])
+		while (s[i] != '\0')
 		{
 			_putchar(s[i]);
+			str_val += 1;
 			i++;
 		}
-		return (i);
+		//return (i);
 	}
+	return (str_val);
 }
