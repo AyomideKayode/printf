22748641eef03ba3c842011664030f7f3b137a15
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-24 23:33:30 +0000

    Hot fixed _binary.c

--- _binary.c
+++ _binary.c
@@ -1,39 +1,51 @@
 #include "main.h"
 
 /**
- * _recursive_binary - print converted integer to binary.
- * @n: integer to be converted to binary.
- *
- * Return: nothing.
- */
-void _recursive_binary(unsigned int n)
-{
-	if (n > 1)
-		_recursive_binary(n / 2);
-	_putchar('0' + n % 2);
-}
-
-/**
  * _print_binary - print the converted integer from
  * arguement to binary.
  * @args_p: argument parameter to convert.
  *
  * Return: length of binary digit
  */
+
 int _print_binary(va_list args_p)
 {
-	unsigned int n;
-	int len;
-
-	n = va_arg(args_p, unsigned int);
+	unsigned int temp;
+	unsigned int n = va_arg(args_p, unsigned int);
+	int len = 0;
+	char *s = NULL;
 
 	if (n == 0)
-		return (0);
+	{
+		_putchar('0');
+		return (1);
+	}
 
-	if (n < 0)
+	temp = n;
+	while (temp > 0)
+	{
+		len++;
+		temp >>= 1;
+	}
+
+	s = malloc(sizeof(char) * (len + 1));
+	if (!s)
 		return (-1);
 
-	len = _base_len(n, 2);
-	_recursive_binary(n);
+	for (int i = len - 1; i >= 0; i--)
+	{
+		s[i] = (n & 1) ? '1' : '0';
+		n >>= 1;
+	}
+
+	s[len] = '\0';
+
+	for (int i = 0; i < len; i++)
+	{
+		_putchar(s[i]);
+	}
+
+	free(s);
 	return (len);
 }
+
