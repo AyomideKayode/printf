9b0bb53721c6a01d9d6f63183772640de35cb91c
Olixc <noibiabdulrasaq@rocketmail.com>
2023-07-24 02:58:30 +0000

    Create directory for all test cases

---
+++ test/_printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - custom printf function that produces output according to a format.
+ * @format: format (char, string, int, etc)
+ *
+ * Return: the number of characters.
+ */
+
+int _printf(const char *format, ...)
+{
+	int n_chars;
+	specifier_t f_array[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_cent},
+		{NULL, NULL}
+	};
+
+	va_list args_p;
+
+	if (!format)
+		return (-1);
+
+	va_start(args_p, format);
+	n_chars = get_function(format, f_array, args_p);
+	va_end(args_p);
+	return (n_chars);
+}
---
+++ test/test_printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int main(void)
+{
+	int rv1, rv2;
+	char olix[] = "Olix and Kayode project";
+	int c = _putchar('c');
+	int c2 = putchar('c');
+	char ap = 'G';
+
+
+	/*test case for normal C printf()*/
+	puts("\nUsing original printf()\n---");
+	rv1 = printf("OlixKay\n");
+	printf("rv is %d\n", rv1);
+	rv1 = printf("%c olix\n", ap);
+	printf("rv1= %d\n", rv1);
+	rv1 = printf("This is %s\n", olix);
+	rv1 = printf("This is %c %s and %s\n", 'a', "kayode", "Olix");
+	printf("rv1 = %d\n", rv1);
+	rv1 = printf("I score 100%% in my jamb and %s score %c 90%%.\n", "Kay", 'a');
+	printf("rv1 = %d\n", rv1);
+
+	/*Test case for customized printf() function*/
+	puts("\nUsing customized printf()\n---");
+	rv2 = _printf("OlixKay\n");
+	printf("rv2 is %d\n", rv2);
+	rv2 = _printf("%c olix\n", ap);
+	printf("rv2= %d\n", rv2);
+	rv2 = _printf("This is %s\n", olix);
+	printf("rv2= %d\n", rv2);
+	rv2 = _printf("This is %c %s and %s\n", 'a', "kayode", "Olix");
+	printf("rv2 = %d\n", rv2);
+	rv2 = _printf("I score 100%% in my jamb and %s score %c 90%%.\n", "Kay", 'a');
+	printf("rv2 = %d\n", rv2);
+}
