7eb27b14d0e70f5747732febf9047843070dab5a
AyomideKayode <ayomidekay7@gmail.com>
2023-07-25 17:49:39 +0000

    Improved documentation for _print_functions.c

--- _print_functions.c
+++ _print_functions.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _print_string - print string.
- * @args_p: Arguement string to print.
+ * _print_string - Handles the '%s' format specifier and prints a string.
+ * @args_p: va_list arguement containing the string to print.
  *
  * Return: length of string.
  */
@@ -14,6 +14,7 @@ int _print_string(va_list args_p)
 	if (s == NULL)
 		s = "(null)";
 
+/* Print each character of the string and update the character count (i)*/
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
@@ -24,10 +25,10 @@ int _print_string(va_list args_p)
 }
 
 /**
- * _print_char -  print single character.
- * @args_p: argument character to print.
+ * _print_char - Handles the '%c' format specifier and print single character.
+ * @args_p: va_list argument containing the character to print.
  *
- * Return: Always 1 success
+ * Return: Always returns 1 (indicating 1 character printed).
  */
 int _print_char(va_list args_p)
 {
@@ -36,38 +37,43 @@ int _print_char(va_list args_p)
 }
 
 /**
- * _print_cent - print percetnage sign.
- * @args_p: argument sign to print.
- * Return: Always 1 success
+ * _print_cent - Handles the '%%' format specifier and print percetnage sign.
+ * @args_p: va_list argument (not used).
+ *
+ * Return: Always returns 1 (indicating 1 character printed).
  */
 int _print_cent(va_list args_p)
 {
-	(void)args_p;
+	(void)args_p; /* Silence unused parameter warning */
 	_putchar('%');
 	return (1);
 }
 
 /**
- * _print_int - print integer.
- * @args_p: argument of integer to print.
- * Return: length of digits printed.
+ * _print_int - Handles the '%d' and '%i' format specifier and print integer.
+ * @args_p: va_list argument containing the integer to print.
+ *
+ * Return: length/number of digits printed.
  */
 int _print_int(va_list args_p)
 {
 	int n;
 
+	/* Call the helper function _print_numbers to handle integer printing*/
 	n = _print_numbers(args_p);
 	return (n);
 }
 /**
- * unsigned_int - Prints Unsigned integers
- * @args_p: argument of unsigned ints
- * Return: length of unsigned digits printed
+ * unsigned_int - Handles the '%u' format specifier and print unsigned integer.
+ * @args_p: va_list argument containing the unsigned integer to print.
+ *
+ * Return: length/number of unsigned digits printed.
  */
 int unsigned_int(va_list args_p)
 {
 	unsigned int n;
 
+	/* Get the unsigned integer from va_list */
 	n = va_arg(args_p, unsigned int);
 
 	if (n == 0)
